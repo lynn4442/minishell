04808ebd62b21fade8cab5cf4af181df711268ba
Lynn Youssef <lyoussef@c1r1s5.42beirut.com>
2025-03-07 21:01:51 +0200

    3

--- src/echo.c
+++ src/echo.c
@@ -6,52 +6,111 @@
 /*   By: lyoussef <lyoussef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 15:39:22 by lyoussef          #+#    #+#             */
-/*   Updated: 2025/03/06 19:45:30 by lyoussef         ###   ########.fr       */
+/*   Updated: 2025/03/07 20:58:53 by lyoussef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char *get_env_value(t_env_var *env_list, char *var_name)
+t_env_var *get_env_var(t_exec *exec, const char *name)
 {
-	while (env_list)
+	t_env_var *current = exec->env_list;
+	while (current)
 	{
-		if (ft_strcmp(env_list->name, var_name) == 0)
-			return env_list->value;
-		env_list = env_list->next;
+		if (strcmp(current->name, name) == 0)
+			return current;
+		current = current->next;
 	}
-	return "";
+	return NULL;
 }
 
-void	ft_echo(t_cmd_node *cmd, t_env_var *env)
+void update_env_var(t_exec *exec, const char *name, const char *value)
 {
-	int		i;
-	int		newline;
-	char	*env_value;
-
-	i = 1;
-	newline = 1;
-	if (!cmd || !cmd->arr || !cmd->arr[0])
-		return ;
-	if (cmd->arr[i] && ft_strcmp(cmd->arr[i], "-n") == 0)
+	t_env_var *var = get_env_var(exec, name);
+	if (var)
 	{
-		newline = 0;
-		i++;
+		free(var->value);
+		var->value = strdup(value);
 	}
-	while (cmd->arr[i])
+	else
 	{
-		if (cmd->arr[i][0] == '$')
+		t_env_var *new_var = malloc(sizeof(t_env_var));
+		new_var->name = strdup(name);
+		new_var->value = strdup(value);
+		new_var->next = exec->env_list;
+		new_var->prev = NULL;
+		if (exec->env_list)
+			exec->env_list->prev = new_var;
+		exec->env_list = new_var;
+	}
+}
+
+int change_directory(const char *path, t_exec *exec)
+{
+	if (!path || *path == '\0')
+	{
+		fprintf(stderr, "cd: OLDPWD not set\n");
+		exec->exit_status = 1;
+		return 1;
+	}
+	if (chdir(path) != 0)
+	{
+		perror("minishell");
+		exec->exit_status = 1;
+		return 1;
+	}
+	return 0;
+}
+
+static void update_pwd_vars(t_exec *exec, const char *old_pwd)
+{
+	char cwd[1024];
+	if (!getcwd(cwd, sizeof(cwd)))
+	{
+		perror("cd");
+		exec->exit_status = 1;
+		return;
+	}
+	update_env_var(exec, "OLDPWD", old_pwd);
+	update_env_var(exec, "PWD", cwd);
+	exec->exit_status = 0;
+}
+
+int execute_cd(t_exec *exec, const char *arg)
+{
+	char *old_pwd;
+	t_env_var *pwd_var = get_env_var(exec, "PWD");
+
+	if (pwd_var)
+	  old_pwd = pwd_var->value;
+	else
+	  old_pwd = "";
+	if (!arg || strcmp(arg, "~") == 0)
+	{
+		t_env_var *home_var = get_env_var(exec, "HOME");
+		if (!home_var || !home_var->value)
 		{
-			env_value = get_env_value(env, cmd->arr[i] + 1);
-			write(1, env_value, ft_strlen(env_value));
+			fprintf(stderr, "cd: HOME not set\n");
+			exec->exit_status = 1;
+			return 1;
 		}
-		else
-			write(1, cmd->arr[i], ft_strlen(cmd->arr[i]));
-
-		if (cmd->arr[i + 1])
-			write(1, " ", 1);
-		i++;
+		arg = home_var->value;
 	}
-	if (newline)
-		write(1, "\n", 1);
+	else if (strcmp(arg, "-") == 0)
+	{
+		t_env_var *oldpwd_var = get_env_var(exec, "OLDPWD");
+		if (!oldpwd_var || !oldpwd_var->value)
+		{
+			fprintf(stderr, "cd: OLDPWD not set\n");
+			exec->exit_status = 1;
+			return 1;
+		}
+		printf("%s\n", oldpwd_var->value);
+		arg = oldpwd_var->value;
+	}
+
+	if (change_directory(arg, exec) == 0)
+		update_pwd_vars(exec, old_pwd);
+
+	return 1;
 }
---
+++ src/export.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyoussef <lyoussef@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 20:45:55 by lyoussef          #+#    #+#             */
+/*   Updated: 2025/03/07 20:48:26 by lyoussef         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../minishell.h"
+
+void swap_env_vars(t_env_var *a, t_env_var *b)
+{
+	char *temp_name;
+	char *temp_value;
+
+	temp_name = a->name;
+	a->name = b->name;
+	b->name = temp_name;
+
+	temp_value = a->value;
+	a->value = b->value;
+	b->value = temp_value;
+}
+
+void sort_env_vars(t_env_var *head)
+{
+	int swapped;
+	t_env_var *ptr1;
+	t_env_var *lptr;
+
+	lptr = NULL;
+	if (head == NULL)
+		return;
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		ptr1 = head;
+
+		while (ptr1->next != lptr)
+		{
+			if (strcmp(ptr1->name, ptr1->next->name) > 0)
+			{
+				swap_env_vars(ptr1, ptr1->next);
+				swapped = 1;
+			}
+			ptr1 = ptr1->next;
+		}
+		lptr = ptr1;
+	}
+}
+
+void export_no_options(t_env_var *env_list)
+{
+	t_env_var *temp;
+
+	temp = env_list;
+	sort_env_vars(env_list);
+	while (temp)
+	{
+		ft_putstr_fd(temp->name, 1);
+		ft_putstr_fd("=", 1);
+		ft_putstr_fd(temp->value, 1);
+		ft_putstr_fd("\n", 1);
+		temp = temp->next;
+	}
+}

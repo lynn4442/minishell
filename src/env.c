3e7698dc9e0bbf23bf07c71dcf78576891d3e0f7
Lynn Youssef <lyoussef@c2r4s1.42beirut.com>
2025-03-08 15:10:36 +0200

    5

---
+++ src/env.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyoussef <lyoussef@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 15:06:43 by lyoussef          #+#    #+#             */
+/*   Updated: 2025/03/08 15:10:15 by lyoussef         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../minishell.h"
+
+void ft_env(t_exec *exec)
+{
+	t_env_var *current;
+
+	if (!exec || !exec->env_list)
+		return ;
+	current = exec->env_list;
+	while (current)
+	{
+		if (current->value)
+			printf("%s=%s\n", current->name, current->value);
+		else
+			printf("%s=\n", current->name);
+		current = current->next;
+	}
+}

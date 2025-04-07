b5ff71c7414112f303ab99814741d434f50e7e65
lynn <lynnyoussef36@gmail.com>
2025-04-07 07:59:38 +0300

    trying to work the push.sh

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Check if a commit message is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 \"commit message\""
+    exit 1
+fi
+# Clean up the build files
+make fclean
+# Git commands
+git add .
+git commit -m "$1"
+git push
+echo ":white_check_mark: Changes pushed successfully!"

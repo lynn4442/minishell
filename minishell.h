59a4db550bb122bb5bc3beae5e9aab13c1199044
HadiHusseiny <hadi.husseiny.lb@gmail.com>
2025-05-26 21:53:20 +0300

    updated header

--- minishell.h
+++ minishell.h
@@ -127,6 +127,15 @@ typedef struct s_exec
 	int			heredoc_counter;  // Counter for unique heredoc filenames
 }	t_exec;
 
+typedef	struct s_quote_check
+{
+	int		i;
+	int		len;
+	char	*res;
+	char	quote_type;
+	int		escaped;
+} t_quote_check;
+
 //init_data
 t_env_var	*create_env_var(t_exec *exec, char *name, char *value);
 t_cmd_node	*create_cmd_node(t_exec *exec, char **args);
@@ -232,6 +241,11 @@ int restore_input_redirection(int original_fd);
 int check_quotes(const char *input);
 void print_with_quote_handling(const char *arg, t_env_var *env, t_exec *exec);
 char *process_quotes(const char *str, t_env_var *env, t_exec *exec);
+int	handle_exit_status(char *res, int len, t_exec *exec);
+char	*extract_var_name(const char *str, int start, int end);
+int	process_quoted_text(const char *str, char **result,t_env_var *env, t_exec *exec);
+int	handle_quote(const char *str, t_quote_check *st);
+int process_special_chars(const char *str, t_quote_check *st, t_env_var *env, t_exec *exec);
 
 // Add these prototypes
 void setup_interactive_signals(void);
